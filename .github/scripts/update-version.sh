f854629b12c5a85677cf97b88655be03f4bbcfac
Anton Melekhin <antmelekhin@gmail.com>
2023-10-16 21:29:26 +0400

    fix script

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -27,8 +27,8 @@ if [[ $CURRENT_VERSION == $LATEST_VERSION ]]; then
 fi
 
 # Update latest version
-sed -i '' "s/_version:.*$/_version: '${LATEST_VERSION}'/" 'defaults/main.yml'
-sed -i '' "s/${CURRENT_VERSION}/${LATEST_VERSION}/" 'README.md'
+sed -i "s/_version:.*$/_version: '${LATEST_VERSION}'/" 'defaults/main.yml'
+sed -i "s/${CURRENT_VERSION}/${LATEST_VERSION}/" 'README.md'
 
 # Repository variables
 REPO_NAME=$(git config --get remote.origin.url | sed -e 's|^https://github.com/||')
