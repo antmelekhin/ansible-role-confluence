3f51958a5da0133ee5c03a36d73bac0794839725
Anton Melekhin <antmelekhin@gmail.com>
2023-10-16 21:27:05 +0400

    fix update commit message

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -33,7 +33,7 @@ sed -i '' "s/${CURRENT_VERSION}/${LATEST_VERSION}/" 'README.md'
 # Repository variables
 REPO_NAME=$(git config --get remote.origin.url | sed -e 's|^https://github.com/||')
 UPDATE_VERSION_BRANCH="update-to-${LATEST_VERSION}"
-UPDATE_VERSION_COMMIT="fix(version): jira updated to \`${LATEST_VERSION}\` release"
+UPDATE_VERSION_COMMIT="fix(version): confluence updated to \`${LATEST_VERSION}\` release"
 
 # Create an update branch
 REMOTE_BRANCH="$(curl --silent https://api.github.com/repos/${REPO_NAME}/branches/${UPDATE_VERSION_BRANCH} | jq -r .name)"
